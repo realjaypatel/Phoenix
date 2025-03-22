6ce0bde6fa7c37be224ff8f2db478570d048cae7
jay <jaykumarpatel@acies.holdings>
2025-03-23 02:52:59 +0530

    v0.101 sanity and error pages

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request, status
+from fastapi.templating import Jinja2Templates
 from routers import business, home,auth,explore
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from fastapi import Request, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
+templates = Jinja2Templates(directory="templates")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -33,6 +35,14 @@ app.include_router(home.router)
 app.include_router(business.router)
 app.include_router(auth.router)
 app.include_router(explore.router)
+app.exception_handler(404)
+async def not_found_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse("error/404.html", {"request": request}, status_code=404)
+
+app.exception_handler(500)
+async def internal_server_error_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse("error/500.html", {"request": request}, status_code=500)
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", port=5000,reload=True)
