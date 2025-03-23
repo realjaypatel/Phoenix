ff6cfae42b2fa9ce2bdf49e0962889024c93dfca
jay <jaykumarpatel@acies.holdings>
2025-03-23 12:44:04 +0530

    a

--- routers/explore.py
+++ routers/explore.py
@@ -1,6 +1,6 @@
 from starlette import status
 from fastapi.templating import Jinja2Templates
-from fastapi import APIRouter, Depends, HTTPException, Path, Request, status
+from fastapi import APIRouter, Depends, HTTPException, Path, Request, status,Query
 import json
 import database
 import requests as rs
@@ -19,28 +19,52 @@ router = APIRouter(
 
 
 
-@router.get('/{search_name}', status_code=status.HTTP_200_OK)
-async def return_home(search_name,request: Request):
+# @router.get('/{search_name}', status_code=status.HTTP_200_OK)
+# async def return_home(search_name,request: Request):
     
+#     user = utils.Validate_User(request)
+#     if user['status'] != 'pass':
+#         user = {'data':{'name':'Sign In'} }   
+#     data = rq.get('https://excel2api.vercel.app/api/1BSOoMb-j3ALwi56lgSW8x7q17iNGSbuq1gpi9vV_ZOQ')
+#     data = data.json()
+#     if not search_name:        
+#         return templates.TemplateResponse("explore.html", {"request": request,"user":user['data'],"data":data})
+#     else:
+#         search_result = []
+#         for value in data:
+#             if search_name.lower() in value['Title'].lower():
+#                 search_result.append(value)
+#         # return templates.TemplateResponse("explore.html", {"request": request,"data":search_result})
+#         return templates.TemplateResponse("explore.html", {"request": request,"user":user['data'],"data":search_result})
+
+    
+
+
+@router.get('/{search_name}', status_code=status.HTTP_200_OK)
+async def return_home(search_name,request: Request,p: int = Query(1, alias="p")):
+    page_size = 20
+    skip = (p - 1) * page_size
+    limit = page_size
+
     user = utils.Validate_User(request)
     if user['status'] != 'pass':
         user = {'data':{'name':'Sign In'} }   
     data = rq.get('https://excel2api.vercel.app/api/1BSOoMb-j3ALwi56lgSW8x7q17iNGSbuq1gpi9vV_ZOQ')
     data = data.json()
-    if not search_name:        
-        return templates.TemplateResponse("explore.html", {"request": request,"user":user['data'],"data":data})
-    else:
-        search_result = []
-        for value in data:
-            if search_name.lower() in value['Title'].lower():
-                search_result.append(value)
-        # return templates.TemplateResponse("explore.html", {"request": request,"data":search_result})
-        return templates.TemplateResponse("explore.html", {"request": request,"user":user['data'],"data":search_result})
-
+    search_result = []
+    for value in data:
+        if search_name.lower() in value['Title'].lower():
+            search_result.append(value)
+    search_result = search_result[skip:skip+limit]
+    return templates.TemplateResponse("explore.html", {"request": request,"user":user['data'],"data":search_result})
+    
     
 
 @router.get('/categories/{category}', status_code=status.HTTP_200_OK)
-async def return_home(request: Request,category):
+async def return_home(request: Request,category,p: int = Query(1, alias="p")):
+    page_size = 20
+    skip = (p - 1) * page_size
+    limit = page_size
     user = utils.Validate_User(request)
     if user['status'] != 'pass':
         user = {'data':{'name':'Sign In'} }   
@@ -53,15 +77,27 @@ async def return_home(request: Request,category):
         for value in data:
             if category.lower() in value['Category'].lower():
                 search_result.append(value)
-        # return templates.TemplateResponse("explore.html", {"request": request,"data":search_result})
+
+        search_result = search_result[skip:skip+limit]
         return templates.TemplateResponse("explore.html", {"request": request,"user":user['data'],"data":search_result})
 
 @router.get('/', status_code=status.HTTP_200_OK)
-async def return_home(request: Request):
+async def return_home(request: Request,p: int = Query(1, alias="p")):
+    page_size = 20
+    skip = (p - 1) * page_size
+    limit = page_size
     
     user = utils.Validate_User(request)
     if user['status'] != 'pass':
         user = {'data':{'name':'Sign In'} }   
     data = rq.get('https://excel2api.vercel.app/api/1BSOoMb-j3ALwi56lgSW8x7q17iNGSbuq1gpi9vV_ZOQ')
     data = data.json()       
+    data = data[skip:skip+limit]
     return templates.TemplateResponse("explore.html", {"request": request,"user":user['data'],"data":data})
+    
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -3,27 +3,7 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from fastapi import FastAPI, Depends, HTTPException, status
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = "your_secret_key"
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
-    return encoded_jwt
-
-# from motor.motor_asyncio import AsyncIOMotorClient
-# client = AsyncIOMotorClient("mongodb+srv://user:user@cluster0.u3fdtma.mongodb.net/Master")
-# db = client["your_database_name"]
 import pymongo
 client = pymongo.MongoClient("mongodb+srv://user:user@cluster0.u3fdtma.mongodb.net/Master")
 db = client["your_database_name"]
